9d43435e72ae44fa5560c88de16b6f9e8b26c0fb
Agnieszka Kurek <agniechakurek@gmail.com>
2016-04-26 13:39:09 +0200

    adding dialogue windows

--- scripts/main.js
+++ scripts/main.js
@@ -7,23 +7,6 @@
  * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
  */
 
-//still need to add semicolon at the end
-2 + 2;
-2 - 2;
-
-//we can perform harder calculations and let javascript do the hard work
-1308480340580845 + 020834028340820348;
-
-//we can multiply
-39993 * 2002;
-
-//divide
-10000000 / 20;
-
-//also have a funny looking thing called modulo
-//divides one value by the second, and returns the leftover amount, or remainder
-10%3; //returns 1 since 3 can go in 3 times, and 1 is leftover
-
-//order of operations
-30 - 2 * 3;
-(30 - 2) * 3; //use parenthesis to make something happen first
+alert("Hello World!");
+confirm("I love JavaScript!");
+prompt("Do you want to lear to JavaScript?");
