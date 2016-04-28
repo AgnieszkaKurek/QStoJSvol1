f883490fa0ec5267185dbbd2929e4646a6f6870f
Agnieszka Kurek <agniechakurek@gmail.com>
2016-04-28 13:15:40 +0200

    creating more complication loop

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,20 @@
-for(var i = 0; i < 11; i++) {
-  console.log(i);
+for(var i = 1; i < 101; i++) {
+  if(i%3 === 0) {
+    
+    if(i%5 === 0) {
+      console.log("fizzbuzz");
+    } 
+    else {
+      console.log("fizz");
+    }
+  }
+   else if (i%5 === 0) {
+    console.log("buzz")
+  }
+  
+  else {
+    console.log(i);
+  }
+ 
 }
+
