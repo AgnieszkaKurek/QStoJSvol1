49e28850227d6e254d432bcc9506b0fe501c0abd
Agnieszka Kurek <agniechakurek@gmail.com>
2016-04-28 17:08:22 +0200

    creating  simple function

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,5 @@
-var sad = confirm ("whod ou like to be cheered up?");
- while (sad) {
-   alert("Whad do you call a boomerang that dosen't come back? ...a stick!");
-   var yes = confirm("would you like to continue our session?")
-   
-   if (yes) {
-     var response = prompt("Tell my about your troubles:", "Type your felling here");
-     if (response) {
-       alert("I am sorry you are feeling down about that. I know you can't see it, but I, the computer, will now give you a hug.  **hug**");
-       sad = false;
-     }
-   } else {
-     sad = false;
-   }
- }
+function calculateArea(side1, side2) {
+ console.log(side1 * side2);
+}
+
+calculateArea(10,40);
