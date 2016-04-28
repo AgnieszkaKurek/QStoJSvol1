30ead478ecfa8018ebdc8bbd70c18d8bda349dd7
Agnieszka Kurek <agniechakurek@gmail.com>
2016-04-28 14:08:23 +0200

    creating more example  while loop

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,15 @@
-var i = 0;
-do {
-  console.log(i);
-  i++; 
-} while (i<11);
-
-while(i < 11) {
-  console.log("i");
-  i++;
-}
-
+var sad = confirm ("whod ou like to be cheered up?");
+ while (sad) {
+   alert("Whad do you call a boomerang that dosen't come back? ...a stick!");
+   var yes = confirm("would you like to continue our session?")
+   
+   if (yes) {
+     var response = prompt("Tell my about your troubles:", "Type your felling here");
+     if (response) {
+       alert("I am sorry you are feeling down about that. I know you can't see it, but I, the computer, will now give you a hug.  **hug**");
+       sad = false;
+     }
+   } else {
+     sad = false;
+   }
+ }
